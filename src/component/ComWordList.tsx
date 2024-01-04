2469aa20e41aaff71ffdb40767c971df34ea122e
wind-ws <1424343223@qq.com>
2024-01-04 22:06:50 +0800

    卧槽,完美解决 列表渲染的问题

--- src/component/ComWordList.tsx
+++ src/component/ComWordList.tsx
@@ -15,89 +15,50 @@ type Props = {
 
 
 const ComWordList = ({ book_name, word_list, on_change_word_list }: Props) => {
-   // todo : 要解决大量数据渲染列表的性能问题
+   // 已解决(不要删除注释,万一当前解决方案有问题,重新开始可以少走点弯路) : 要解决大量数据渲染列表的性能问题
    //    不能用react-virtualized,因为很难做到局部渲染,第二是它的单行高度不能自动调整
    //    既然虚表都无法调整高度(不美观啦~没办法啦~),那么只能用 分片渲染了
    //    memo
 
-   // const [i, set_i] = useState(-1);
-   // const [isPending, startTransition] = useTransition();
-   // const i = useRef(word_list.length > 100 ? 100 : word_list.length);
-   // const [list, set_list] = useState<string[]>(() => {
-   //    const inter_id = setInterval(() => {
-   //       console.warn(i.current);
-   //       i.current = i.current + 100;
-   //       if (i.current > word_list.length) {
-   //          i.current = word_list.length;
-   //          clearInterval(inter_id);
-   //       }
-   //       set_list(word_list.slice(0, i.current));
-   //    }, 10);
-   //    return word_list.slice(0, i.current);
-   // });
 
-
-   // const row = ({
-   //    index,
-   // }: {
-   //    index: number
-   // }) => {
-   //    const word_name = word_list[index];
-   //    console.log(`${index}:${word_name}被渲染`);
-   //    const [b, set_b] = useState(false);
-   //    return <div className="flex flex-col w-full p-2" onClick={() => {
-
-   //    }}>
-   //       <div className="flex">
-   //          {word_name}
-   //       </div>
-   //       <div className="flex ">
-   //          {
-   //             b ? <pre>a
-   //                b
-   //                c</pre> : <></>
-   //          }
-   //       </div>
-   //    </div>
-   // };
-   // const Row = memo(row);
-
-   debug_time.start()
    // 我尼玛!我不是天才谁是天才!完美的列表渲染解决方案
-   // 加载1000个7秒,加载4000个70秒, 不知道为什么越后面越慢
    const RowLoop = memo(({
       index,
    }: {
       index: number
    }) => {
-      const word_name = word_list[index];
-      // console.log(`${index}:${word_name}被渲染`);
-      if(index === word_list.length-1){
-         console.warn(word_name);
-         debug_time.log();
-         debug_time.stop();
-      }
-      if(index%100 == 0){
-         debug_time.log(index+"");
-      }
-      const [b, set_b] = useState(() => {
-         setTimeout(() => {
+      const [b, set_b] = useState(() => {//用于控制是否立马渲染下一个列表
+         const t = 200; // t越高渲染的越快,不建议高于500,不然可能会导致初始加载出现一点点卡顿
+         if (index % t != 0) {//我尼玛!我天才!!神级优化! 2秒渲染完7000个列表
+            return true
+         }
+         setTimeout(() => {//用于停止渲染后再加载渲染,这样的异步控制可以让 页面快速渲染出来,后面的列表再慢慢渲染
             set_b(true)
          });
          return false
       });
-      const [a, set_a] = useState(() =>false);
-      return <>
-         <div className="flex flex-col w-full p-2" onClick={() => {
+      const Row = memo(({//这个memo是为了防止b变化时重新渲染一次列表内容
+         index,
+      }: {
+         index: number
+      }) => {
+         const word_name = word_list[index];
+         const [a, set_a] = useState(() => false);
+         console.log(`${index}:${word_name}被渲染`);
+
+         return <div className="flex flex-col w-full p-2" onClick={() => {
             set_a(!a);
          }}>
             <div className="flex">
                {word_name}
             </div>
             <div className="flex">
-               {a?"bbb":<></>}
+               {a ? "bbb" : <></>}
             </div>
          </div>
+      })
+      return <>
+         <Row index={index}></Row>
          {
             index + 1 < word_list.length && b
                ? <RowLoop index={index + 1}></RowLoop>
@@ -108,11 +69,6 @@ const ComWordList = ({ book_name, word_list, on_change_word_list }: Props) => {
 
 
    return <div className="flex flex-col w-full h-full">
-      {/* {
-         list.map((v, index) => {
-            return <Row key={index} index={index}></Row>
-         })
-      } */}
       <RowLoop index={0}></RowLoop>
    </div>
 }
