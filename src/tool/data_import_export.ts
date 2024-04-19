91e093f735939bb6a137c56c8b99811edbcc6f6f
wind-ws <1424343223@qq.com>
2024-04-19 21:46:17 +0800

    tauri-plugin-dialog 版本变更 (检查安全)
    "2.0.0-beta.3" -> "2.0.0-beta.5"

--- src/tool/data_import_export.ts
+++ src/tool/data_import_export.ts
@@ -1,10 +1,12 @@
 // 数据导入导出
 
+import { fetch } from "@tauri-apps/plugin-http";
 import { save_export_file } from "./fs";
 import { store_book, store_global } from "./store/book";
 import { store_setting } from "./store/setting";
 import { store_state } from "./store/state";
 import { open } from '@tauri-apps/plugin-dialog';
+import { readTextFile } from "@tauri-apps/plugin-fs";
 
 /** 当前的数据版本,版本号不与APP版本完全同步,只在数据变更后,同步到APP版本 */
 const version = "0.3";
@@ -44,14 +46,20 @@ export function export_data(configuration?: any | { file_name: string }) {
 /** 0.3 */
 export namespace version_0_3 {
 
-   export const  _import: ImportFn = async ( configuration?: any) => {
+   // 问题追踪: https://github.com/tauri-apps/tauri/issues/9083
+   //    https://github.com/tauri-apps/tauri/pull/9311
+   // 似乎在新版本被解决了
+   export const _import: ImportFn = async (configuration?: any) => {
       // const version = data.version;
       const file = await open({
          multiple: false,
          directory: false,
       })
       console.log(file);
-
+      const path: string = file?.path as any;
+      const a = await readTextFile(path);
+      console.log(a);
+      
       // 若存在下一个版本,则 类型变换后,调用下个版本的_import函数      
    }
 
